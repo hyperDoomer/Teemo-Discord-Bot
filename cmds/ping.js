67042c4273857af0d032e194ea986fdc54468e88
hyperdoomer <zhemna.v@gmail.com>
2019-07-29 03:02:35 +0300

    1

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,8 +1,8 @@
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
-  cooldown: 5,
-  aliases: ['pingg'],
+  	cooldown: 2,
+  	aliases: ['pingg'],
 	execute(bot, message, args) {
 		message.channel.send('Pong.');
 	},
