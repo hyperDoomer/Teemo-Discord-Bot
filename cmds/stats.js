f02137535a61dea9bbc78163b7b164c41ced6033
hyperdoomer <zhemna.v@gmail.com>
2019-08-09 23:03:35 +0300

    stats

--- cmds/stats.js
+++ cmds/stats.js
@@ -2,10 +2,9 @@ const Discord = module.require("discord.js");
 module.exports = {
 	name: 'stats',
 	description: 'Bot statistics',
-  	cooldown: 3,
+  	cooldown: 10,
   	aliases: ['st'],
 	execute(bot, message, args) {
-let g = bot.guilds.size;
-message.author.send("Guilds size= "+g);
+		console.log(bot.guilds.map(i => `${i.name}: ${i.id}`).join("\n"))
 	},
 };
