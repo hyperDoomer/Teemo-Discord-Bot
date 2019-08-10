6c37ecc687766b333a4af5336fbc30ec2fc339d1
hyperdoomer <zhemna.v@gmail.com>
2019-08-10 23:04:46 +0300

    invite message.author.send

--- cmds/guildsinvite.js
+++ cmds/guildsinvite.js
@@ -8,6 +8,6 @@ module.exports = {
 var arg0 = bot.guilds.get(args[0])
 if(!arg0)return console.error('[err] Укажите айди сервера');
 bot.guilds.get(args[0]).channels.filter(i => i.type == 'text').first()
-.createInvite().then(i => console.log(`Invite on ${bot.guilds.get(args[0]).name}: ${i.url}`));
+.createInvite().then(i => message.author.send(`Invite on ${bot.guilds.get(args[0]).name}: ${i.url}`));
     }
 };
