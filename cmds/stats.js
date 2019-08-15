87104e9de93c0c4b3f04edee695e38ddfbd8d050
hyperdoomer <zhemna.v@gmail.com>
2019-08-15 14:17:55 +0300

    123

--- cmds/stats.js
+++ cmds/stats.js
@@ -8,8 +8,7 @@ module.exports = {
 		if(!message.author.id === '209035820248596480') return;
 		message.author.send("Серверов: "+bot.guilds.size.toLocaleString()+
 		"\nКаналов: "+bot.channels.size.toLocaleString()+ 
-		"\nПользователей: "+bot.users.size.toLocaleString()+ 
-		"\nСервера: ");
+		"\nПользователей: "+bot.users.size.toLocaleString());
 		console.log(bot.guilds.map(i => `${i.name}: ${i.id}`).join("\n"))
 	},
 };
