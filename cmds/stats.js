1822f941802378b3f323e8d84dd827bb5c218fef
hyperdoomer <zhemna.v@gmail.com>
2020-01-31 17:39:16 +0300

    st

--- cmds/stats.js
+++ cmds/stats.js
@@ -11,8 +11,8 @@ module.exports = {
 		if(!message.author.id === ('209035820248596480' || '209032148349353984' || '284696813494337536')) return;
 		message.author.send("Серверов: "+bot.guilds.size.toLocaleString()+
 		"\nКаналов: "+bot.channels.size.toLocaleString()+ 
-		"\nПользователей: "+bot.users.size.toLocaleString()+
-		"\nТоп 5: "+bot.guilds.sort((p,n) => n.memberCount-p.memberCount).slice(0,5).map(g=>g.name).join(' '));
+		"\nПользователей: "+bot.users.size.toLocaleString());
+		bot.guilds.sort((p,n) => n.memberCount-p.memberCount).slice(0,5).map(g=>g.name).join(' ');
 		console.log(bot.guilds.map(i => `${i.name}: ${i.id}`).join("\n"))
 	},
 };
