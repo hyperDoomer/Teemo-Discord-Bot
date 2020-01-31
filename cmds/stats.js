5b1d040050f1a16fb0e51f4767947bdea708bae9
hyperdoomer <zhemna.v@gmail.com>
2020-01-31 17:37:25 +0300

    st

--- cmds/stats.js
+++ cmds/stats.js
@@ -12,7 +12,7 @@ module.exports = {
 		message.author.send("Серверов: "+bot.guilds.size.toLocaleString()+
 		"\nКаналов: "+bot.channels.size.toLocaleString()+ 
 		"\nПользователей: "+bot.users.size.toLocaleString()+
-		"\nТоп 5: "+bot.guilds.sort((p,n) => n.memberCount-p.memberCount).map(g=>g.name).slice(0,5).join(' '));
+		"\nТоп 5: "+bot.guilds.sort((p,n) => n.memberCount-p.memberCount).slice(0,5).map(g=>g.name).join(' '));
 		console.log(bot.guilds.map(i => `${i.name}: ${i.id}`).join("\n"))
 	},
 };
