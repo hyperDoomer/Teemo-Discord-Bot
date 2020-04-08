be15a0faeab4e4af7b617df4c9e29056f5b3b558
hyperdoomer <zhemna.v@gmail.com>
2020-04-08 16:27:58 +0300

    -tft

--- cmds/playerinfo.js
+++ cmds/playerinfo.js
@@ -74,8 +74,10 @@ if(!json){
 let playeridurl = (`https://${region}.api.riotgames.com/lol/league/v4/entries/by-summoner/${json.id+'?api_key='+riotkey}`);
 const jsonid = await request({uri: playeridurl, json: true});
 //TFT 
+/*
 let tftplayeridurl = (`https://${region}.api.riotgames.com/tft/league/v1/entries/by-summoner/${json.id+'?api_key='+riotkey}`)
 const tftjsonid = await request({uri: tftplayeridurl, json: true});
+*/
 
 //SUMMONER MASTERY
 let playermsturl = (`https://${region}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${json.id+'?api_key='+riotkey}`);
@@ -170,12 +172,13 @@ embed.addField('<:iconvictory231412342134213444:605430936900993035>__RANKED FLEX
 };
 
 //TFT
+/*
 var ranktft = tftjsonid.find(n => n.queueType === 'RANKED_TFT');
 if(!ranktft) {embed.addField('<:iconvictory2123123123214154545:605431423351914499>__RANKED TFT__<:iconvictory2123123123214154545:605431423351914499>', "<:unranked123563425435234523453245:603727489680015360>**UNRANKED**<:unranked123563425435234523453245:603727489680015360>", true);} else{
 var emojiranktft = bot.emojis.find(e => e.name === `${ranktft.tier}` && ['401153786674151424'].includes(e.guild.id));
 let tftwinrate = (100*(ranktft.wins/(ranktft.wins+ranktft.losses))).toFixed(1);
 embed.addField('<:iconvictory2123123123214154545:605431423351914499>__RANKED TFT__<:iconvictory2123123123214154545:605431423351914499>', emojiranktft + "**"+ranktft.tier + ' ' + ranktft.rank+"**" +" "+ranktft.leaguePoints+'LP'+ emojiranktft +'\n **Wins**: '+ranktft.wins+' **Losses**: '+ranktft.losses+'\n **Winrate**: '+tftwinrate+"%"  ,true);}; 
-
+*/
 
 //
 embed.setFooter('Invite Teemo to your server !t invite', 'https://cdn.glitch.com/a90c18e0-56f5-4217-a721-f16cb4d618d4%2F190-1900167_super-teemo.png?v=1563898152547');
